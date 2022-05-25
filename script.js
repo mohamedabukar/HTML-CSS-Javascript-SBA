ecced3bbd12f5c0e7d008be78973910b3073310a
Mohamed <mohamed.h.abukar@gmail.com>
2022-05-25 15:53:11 -0500

    functionality completed for weather api

---
+++ script.js
@@ -0,0 +1,61 @@
+const userSearch = localStorage.getItem("search")
+
+const apiKey = "5d1b3f150d64e1e1cd5a4bedbf160469"
+
+function storeInput(cityName) {
+    const apiUrlcitySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    fetch(apiUrlcitySearch)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            var dt = data.dt;
+            var date = new Date(dt * 1000).toLocaleDateString();
+            $(".city-nme").text(data.name);
+            $(".date").text(date);
+            $(".tempEl").text("Temp: " + (((data.main.temp - 273.15) * 9 / 5) + 32).toFixed(2) + "F");
+            $(".humidityEl").text("Humiditiy: " + data.main.humidity + "%");
+            $(".windEl").text("Wind speed: " + (data.wind.speed) + " mph");
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            fiveDay(lat, lon);
+        })
+
+}
+$(".btn").on("click", function (event) {
+    var cityName = event.target.value;
+    storeInput(cityName);
+})
+
+function fiveDay(lat, lon) {
+    var fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(fiveDayUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 1; i < 6; i++) {
+                var dt = data.daily[i].dt;
+                var date = new Date(dt * 1000).toLocaleDateString("en-US");
+                var weatherForecastHtml = `
+                    <div class="col-2 day">
+                    <h2 class="date">${date}</h2>
+                    <p class="temp">Temp: ${(((data.daily[i].temp.day - 273.15) * 9 / 5) + 32).toFixed(2)}F</p>
+                    <p class="humidity">Humidity: ${data.daily[i].humidity}%</p>
+                    <p class="wind">Wind: ${data.daily[i].wind_speed} MPH</p>
+                </div>
+                `
+                $(".Days").append(weatherForecastHtml);
+
+            }
+        })
+
+}
+
+$(".searchBtn").on("click", function () {
+    let x = $("#city-name").val()
+    storeInput(x);
+
+})
